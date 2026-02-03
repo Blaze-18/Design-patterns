0d4c7d956c1a0057af9693bb5ef041e6ff309aee
Shahriar Anan <110927484+Blaze-18@users.noreply.github.com>
2026-02-03 16:10:32 +0600

    Delete Chain of Responsibillity Pattern/Money Withdrawl System/src directory

--- Chain of Responsibillity Pattern/Money Withdrawl System/src/handlers/Handler.java
+++
@@ -1,8 +0,0 @@
-package handlers;
-
-import request.MoneyWithdrawlRequest;
-
-public interface Handler {
-    public void process(MoneyWithdrawlRequest moneyWithdrawlRequest);
-    public void setNext(Handler nextHandler);
-}
